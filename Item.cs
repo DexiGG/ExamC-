e01411705327e8732ec911a9d2dec14e5433b626
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-04-11 21:40:25 +0600

    Exam

---
+++ Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AddItem
+{   [Serializable]
+    public delegate void KeyPressEvent(object sender);
+    public class Item
+    {
+        public event KeyPressEvent keyPress;
+
+        public int CntItems { get; set; }
+        public string NameItems { get; set; }
+        public double PriceItems { get; set; }
+
+
+        public void AddItems(int cntItem, int priceItem, string nameItem)
+        {
+            CntItems = cntItem;
+            NameItems = nameItem;
+            PriceItems = priceItem;
+            keyPress?.Invoke("Добавлены данные о товаре");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.IO;
+using ClassLibrary1;
+
+namespace AddItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите кол-во товара");
+            int cntItem = int.Parse(Console.ReadLine());
+            Console.WriteLine("ВВедите название товара");
+            string nameItem = Console.ReadLine();
+            Console.WriteLine("ВВедите цену товара");
+            int priceItem = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Для добавления товара нажмите 2xSpace");
+            Item item = new Item();
+
+            ConsoleKeyInfo pressed;
+            pressed = Console.ReadKey();
+           
+
+
+            if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+            {
+                item.AddItems(cntItem, priceItem, nameItem);
+                XmlSerializer format = new XmlSerializer(typeof(Item));
+
+                string path = (@"C:\Item.xml");
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    format.Serialize(fs, item);
+                }
+                Console.WriteLine("Ура добавили");
+            }
+
+
+
+        }
+    }
+}
